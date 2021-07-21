4e9f3a4dc87711a4fbf84d064d0dce2a185d7a47
Erick Kiminza <kiminzaeric@gmail.com>
2021-07-21 07:23:54 +0300

    fix betty by adding function description

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts a node as the left-child of another node
+ * binary_tree_insert_right - inserts a node as the left-child of another node
  *
  * @parent: pointer to the node to insert te left-child in
  * @value: the value to store in the new node
